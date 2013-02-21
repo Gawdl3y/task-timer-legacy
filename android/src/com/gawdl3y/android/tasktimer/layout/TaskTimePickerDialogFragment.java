d4fab34993974cb475ef2823fc576558723c876a
Gawdl3y <Gawdl3y@gmail.com>
2013-02-21 10:56:07 -0500

    added TaskTimePickerDialogFragment.java

---
+++ android/src/com/gawdl3y/android/tasktimer/layout/TaskTimePickerDialogFragment.java
@@ -0,0 +1,7 @@
+package com.gawdl3y.android.tasktimer.layout;
+
+import com.actionbarsherlock.app.SherlockDialogFragment;
+
+public class TaskTimePickerDialogFragment extends SherlockDialogFragment {
+
+}
