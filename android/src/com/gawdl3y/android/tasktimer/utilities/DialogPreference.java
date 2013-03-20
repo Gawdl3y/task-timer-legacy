39784858bc866ef996f822e370bb8c6afda1f8a2
Gawdl3y <Gawdl3y@gmail.com>
2013-03-20 19:41:04 -0400

    added about to preferences (and removed menu item)

---
+++ android/src/com/gawdl3y/android/tasktimer/utilities/DialogPreference.java
@@ -0,0 +1,20 @@
+package com.gawdl3y.android.tasktimer.utilities;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * A wrapper for the built-in DialogPreference
+ * <p>This implementation does absolutely nothing.</p>
+ */
+public class DialogPreference extends android.preference.DialogPreference {
+    /**
+     * Constructor
+     * @param context The context for the DialogPreference
+     * @param attrs The attribute set for the DialogPreference
+     * @see android.preference.DialogPreference#DialogPreference(android.content.Context, android.util.AttributeSet)
+     */
+    public DialogPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
