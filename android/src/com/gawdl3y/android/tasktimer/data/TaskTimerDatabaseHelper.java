ab4e22d6da774e021038d8f8452110b4523d52fe
Gawdl3y <Gawdl3y@gmail.com>
2013-03-25 12:11:09 -0400

    work on provider/db helper

---
+++ android/src/com/gawdl3y/android/tasktimer/data/TaskTimerDatabaseHelper.java
@@ -0,0 +1,156 @@
+package com.gawdl3y.android.tasktimer.data;
+
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import com.gawdl3y.android.tasktimer.pojos.Group;
+import com.gawdl3y.android.tasktimer.pojos.Task;
+import com.gawdl3y.android.tasktimer.util.Log;
+
+public class TaskTimerDatabaseHelper extends SQLiteOpenHelper {
+    public static final String TAG = "TaskTimerDatabaseHelper";
+
+    // General database info
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "TaskTimer.db";
+
+    // Task table
+    private static final String TASKS_TABLE_NAME = "tasks";
+    private static final String TASKS_TABLE_CREATE = "CREATE TABLE "
+            + TASKS_TABLE_NAME + " ("
+            + Task.Columns._ID + " INTEGER, "
+            + Task.Columns.NAME + " TEXT, "
+            + Task.Columns.DESCRIPTION + " TEXT, "
+            + Task.Columns.TIME + " TEXT, "
+            + Task.Columns.GOAL + " TEXT, "
+            + Task.Columns.INDEFINITE + " INTEGER, "
+            + Task.Columns.COMPLETE + " INTEGER, "
+            + Task.Columns.SETTINGS + " TEXT, "
+            + Task.Columns.POSITION + " INTEGER, "
+            + Task.Columns.GROUP + " INTEGER);";
+
+    // Group table
+    private static final String GROUPS_TABLE_NAME = "groups";
+    private static final String GROUPS_TABLE_CREATE = "CREATE TABLE "
+            + GROUPS_TABLE_NAME + " ("
+            + Group.Columns._ID + " INTEGER, "
+            + Group.Columns.NAME + " TEXT"
+            + Group.Columns.POSITION + " INTEGER);";
+
+    /**
+     * Constructor
+     * @param context The context for the database
+     */
+    TaskTimerDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * The database is first being created
+     * @param db The database
+     * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TASKS_TABLE_CREATE);
+        db.execSQL(GROUPS_TABLE_CREATE);
+    }
+
+    /**
+     * The database is being upgraded
+     * @param db The database
+     * @param oldVersion The old database version
+     * @param newVersion The new database version
+     * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Nothing to upgrade from yet!
+    }
+
+
+    /**
+     * Insert a task
+     * @param values The values of the row
+     * @return The URI for the new insertion
+     */
+    public Uri taskInsert(ContentValues values) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        long rowID = -1;
+
+        // Insert the row
+        try {
+            // Make sure the ID is unique
+            Object value = values.get(Task.Columns._ID);
+            if(value != null) {
+                int id = (Integer) value;
+                if(id > -1) {
+                    final Cursor cursor = db.query("tasks", new String[]{Task.Columns._ID}, "_id = ?", new String[] {Integer.toString(id)} , null, null, null);
+                    if(cursor.moveToFirst()) {
+                        // It exists! Remove the ID so SQLite fills in a new one
+                        values.putNull(Task.Columns._ID);
+                    }
+                }
+            }
+
+            // Perform the insertion
+            rowID = db.insert("tasks", Task.Columns.NAME, values);
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
+        // Insertion failure
+        if(rowID < 0) throw new SQLException("Failed to insert row");
+
+        Log.v(TAG, "Added new task with ID " + rowID);
+        return ContentUris.withAppendedId(Task.Columns.CONTENT_URI, rowID);
+    }
+
+
+    /**
+     * Insert a group
+     * @param values The values of the row
+     * @return The URI for the new insertion
+     */
+    public Uri groupInsert(ContentValues values) {
+        SQLiteDatabase db = getWritableDatabase();
+        db.beginTransaction();
+        long rowID = -1;
+
+        // Insert the row
+        try {
+            // Make sure the ID is unique
+            Object value = values.get(Group.Columns._ID);
+            if(value != null) {
+                int id = (Integer) value;
+                if(id > -1) {
+                    final Cursor cursor = db.query("groups", new String[]{Group.Columns._ID}, "_id = ?", new String[]{Integer.toString(id)} , null, null, null);
+                    if(cursor.moveToFirst()) {
+                        // It exists! Remove the ID so SQLite fills in a new one
+                        values.putNull(Group.Columns._ID);
+                    }
+                }
+            }
+
+            // Perform the insertion
+            rowID = db.insert("groups", Task.Columns.NAME, values);
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
+        }
+
+        // Insertion failure
+        if(rowID < 0) throw new SQLException("Failed to insert row");
+
+        Log.v(TAG, "Added new group with ID " + rowID);
+        return ContentUris.withAppendedId(Task.Columns.CONTENT_URI, rowID);
+    }
+}
--- android/src/com/gawdl3y/android/tasktimer/data/TaskTimerProvider.java
+++ android/src/com/gawdl3y/android/tasktimer/data/TaskTimerProvider.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 import com.gawdl3y.android.tasktimer.util.Log;
 
 /**
@@ -24,7 +25,7 @@ public class TaskTimerProvider extends ContentProvider {
 
     private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-    private TaskTimerOpenHelper openHelper;
+    private TaskTimerDatabaseHelper dbHelper;
 
     static {
         uriMatcher.addURI("com.gawdl3y.android.tasktimer", "tasks", TASKS);
@@ -40,7 +41,7 @@ public class TaskTimerProvider extends ContentProvider {
      */
     @Override
     public boolean onCreate() {
-        openHelper = new TaskTimerOpenHelper(getContext());
+        dbHelper = new TaskTimerDatabaseHelper(getContext());
         return true;
     }
 
@@ -69,7 +70,7 @@ public class TaskTimerProvider extends ContentProvider {
 
     /**
      * Perform a query
-     * @param uri           The URI of the query
+     * @param url           The URI of the query
      * @param projection    The list of columns to put into the cursor, or all columns if null
      * @param selection     Selection to restrict to, or all rows if null
      * @param selectionArgs Values to replace placeholders with in selection
@@ -78,66 +79,41 @@ public class TaskTimerProvider extends ContentProvider {
      * @see android.content.ContentProvider#query(android.net.Uri, String[], String, String[], String)
      */
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(Uri url, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
-        int match = uriMatcher.match(uri);
+        int match = uriMatcher.match(url);
         switch(match) {
             case TASKS:
                 qb.setTables("tasks");
                 break;
             case TASKS_ID:
                 qb.setTables("tasks");
-                qb.appendWhere("_ID=");
-                qb.appendWhere(uri.getPathSegments().get(1));
+                qb.appendWhere("_id=");
+                qb.appendWhere(url.getPathSegments().get(1));
                 break;
             case GROUPS:
                 qb.setTables("groups");
                 break;
             case GROUPS_ID:
                 qb.setTables("groups");
-                qb.appendWhere("_ID=");
-                qb.appendWhere(uri.getPathSegments().get(1));
+                qb.appendWhere("_id=");
+                qb.appendWhere(url.getPathSegments().get(1));
                 break;
             default:
-                throw new IllegalArgumentException("Unknown URI " + uri);
+                throw new IllegalArgumentException("Unknown URL " + url);
         }
 
-        SQLiteDatabase db = openHelper.getReadableDatabase();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
         Cursor cursor = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
 
-        if(cursor == null) Log.v(TAG, "Query failed: "); else cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        if(cursor == null) Log.v(TAG, "Query failed"); else cursor.setNotificationUri(getContext().getContentResolver(), url);
         return cursor;
     }
 
     /**
-     * Insert a row
-     * @param uri    The URI for the query
-     * @param values The values to insert
-     * @return The URI for the inserted items
-     * @see android.content.ContentProvider#insert(android.net.Uri, android.content.ContentValues)
-     */
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        return null;
-    }
-
-    /**
-     * Delete row(s)
-     * @param uri           The URI for the query
-     * @param selection     The selection to restrict to
-     * @param selectionArgs Values to replace placeholders with in selection
-     * @return The number of rows affected
-     * @see android.content.ContentProvider#delete(android.net.Uri, String, String[])
-     */
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
-
-    /**
      * Update row(s)
-     * @param uri           The URI for the query
+     * @param url           The URI for the query
      * @param values        The values to update
      * @param selection     The selection to restrict to
      * @param selectionArgs Values to replace placeholders with in selection
@@ -145,7 +121,93 @@ public class TaskTimerProvider extends ContentProvider {
      * @see android.content.ContentProvider#update(android.net.Uri, android.content.ContentValues, String, String[])
      */
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+    public int update(Uri url, ContentValues values, String selection, String[] selectionArgs) {
+        int match = uriMatcher.match(url), count;
+        long rowID;
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch(match) {
+            case TASKS_ID:
+                String segment = url.getPathSegments().get(1);
+                rowID = Long.parseLong(segment);
+                count = db.update("tasks", values, "_id=" + rowID, null);
+                break;
+            default:
+                throw new UnsupportedOperationException("Cannot update URL: " + url);
+        }
+
+        Log.v(TAG, "*** notifyChange() rowID: " + rowID + " url " + url);
+        getContext().getContentResolver().notifyChange(url, null);
+        return count;
+    }
+
+    /**
+     * Insert a row
+     * @param url    The URI for the query
+     * @param values The values to insert
+     * @return The URI for the inserted items
+     * @see android.content.ContentProvider#insert(android.net.Uri, android.content.ContentValues)
+     */
+    @Override
+    public Uri insert(Uri url, ContentValues values) {
+        int match = uriMatcher.match(url);
+        Uri newURL;
+
+        switch(match) {
+            case TASKS:
+                newURL = dbHelper.taskInsert(values);
+                break;
+            case GROUPS:
+                newURL = dbHelper.groupInsert(values);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot insert into URL: " + url);
+        }
+
+        getContext().getContentResolver().notifyChange(newURL, null);
+        return newURL;
+    }
+
+    /**
+     * Delete row(s)
+     * @param url           The URI for the query
+     * @param selection     The selection to restrict to
+     * @param selectionArgs Values to replace placeholders with in selection
+     * @return The number of rows affected
+     * @see android.content.ContentProvider#delete(android.net.Uri, String, String[])
+     */
+    @Override
+    public int delete(Uri url, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int count;
+        long rowID = 0;
+        String segment;
+
+        switch(uriMatcher.match(url)) {
+            case TASKS:
+                count = db.delete("tasks", selection, selectionArgs);
+                break;
+            case TASKS_ID:
+                segment = url.getPathSegments().get(1);
+                rowID = Long.parseLong(segment);
+                selection = "_id=" + segment + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ")" : "");
+                count = db.delete("tasks", selection, selectionArgs);
+                break;
+
+            case GROUPS:
+                count = db.delete("groups", selection, selectionArgs);
+                break;
+            case GROUPS_ID:
+                segment = url.getPathSegments().get(1);
+                rowID = Long.parseLong(segment);
+                selection = "_id=" + segment + (!TextUtils.isEmpty(selection) ? " AND (" + selection + ")" : "");
+                count = db.delete("groups", selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot delete from URL: " + url);
+        }
+
+        getContext().getContentResolver().notifyChange(url, null);
+        return count;
     }
 }
