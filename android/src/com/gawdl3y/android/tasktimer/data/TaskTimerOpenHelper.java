f7016b45b58b81642c1b272cb1dfc1ac489850af
Gawdl3y <Gawdl3y@gmail.com>
2013-03-25 10:34:55 -0400

    added javadocs to TaskTimerOpenHelper and changed some fields

--- android/src/com/gawdl3y/android/tasktimer/data/TaskTimerOpenHelper.java
+++ android/src/com/gawdl3y/android/tasktimer/data/TaskTimerOpenHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 public class TaskTimerOpenHelper extends SQLiteOpenHelper {
-
+    // General database info
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "TaskTimer";
 
@@ -25,7 +25,7 @@ public class TaskTimerOpenHelper extends SQLiteOpenHelper {
     private static final String TASKS_TABLE_NAME = "tasks";
     private static final String TASKS_TABLE_CREATE = "CREATE TABLE "
             + TASKS_TABLE_NAME + " ("
-            + TASK_ID + " TEXT, "
+            + TASK_ID + " INTEGER, "
             + TASK_NAME + " TEXT, "
             + TASK_DESCRIPTION + " TEXT, "
             + TASK_TIME + " TEXT, "
@@ -40,22 +40,38 @@ public class TaskTimerOpenHelper extends SQLiteOpenHelper {
     private static final String GROUP_NAME = "name";
 
     // Group table
-    private static final String GROUPS_TABLE_NAME = "categories";
+    private static final String GROUPS_TABLE_NAME = "groups";
     private static final String GROUPS_TABLE_CREATE = "CREATE TABLE "
             + GROUPS_TABLE_NAME + " ("
             + GROUP_ID + " INTEGER, "
             + GROUP_NAME + " TEXT);";
 
+    /**
+     * Constructor
+     * @param context The context for the database
+     */
     TaskTimerOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    /**
+     * The database is first being created
+     * @param db The database
+     * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+     */
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TASKS_TABLE_CREATE);
         db.execSQL(GROUPS_TABLE_CREATE);
     }
 
+    /**
+     * The database is being upgraded
+     * @param db The database
+     * @param oldVersion The old database version
+     * @param newVersion The new database version
+     * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
